82a6fe2b3b63fcd95d83e31317643899a0f4ffab
llb0 <108737112+llb0@users.noreply.github.com>
2026-08-18 15:42:41 +0800

    Refactor parse method call in PlayerManager

--- app/src/main/java/com/fongmi/android/tv/player/PlayerManager.java
+++ app/src/main/java/com/fongmi/android/tv/player/PlayerManager.java
@@ -4506,8 +4506,8 @@ public class PlayerManager implements ParseCallback {
         if (desiredKernel != playerType) {
             final Integer target = desiredKernel;
             App.post(() -> switchPlayer(target, false), 200);
+        }
     }
-
     public void parse(String key, Result result, boolean useParse, MediaMetadata metadata) {
         parse(key, result, useParse, metadata, true);
     }
