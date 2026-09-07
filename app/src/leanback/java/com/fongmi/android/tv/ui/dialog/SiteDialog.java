964cfb7f775b9a47ada8ecb233fb2c100497a9d7
llb0 <108737112+llb0@users.noreply.github.com>
2026-09-07 22:06:21 +0800

    Remove null check for activity in Toast message

--- app/src/leanback/java/com/fongmi/android/tv/ui/dialog/SiteDialog.java
+++ app/src/leanback/java/com/fongmi/android/tv/ui/dialog/SiteDialog.java
@@ -313,7 +313,7 @@ public class SiteDialog extends BaseAlertDialog implements SiteAdapter.OnClickLi
         if (subDir.isEmpty()) return;
         File file = new File(Path.root() + "/tvbox/" + subDir, fileName);
         if (file.exists()) file.delete();
-        if (activity != null) Toast.makeText(requireActivity(), getString(R.string.setting_site_delete_done, item.getName()), Toast.LENGTH_SHORT).show();
+        Toast.makeText(requireActivity(), getString(R.string.setting_site_delete_done, item.getName()), Toast.LENGTH_SHORT).show();
         if (adapter != null) adapter.filter(null);
     }
 
