318067b5c0f6d3a401c4c06ca67e9b35e92cdb80
llb0 <108737112+llb0@users.noreply.github.com>
2026-09-09 18:24:39 +0800

    Refactor CustomTitleView to simplify key handling

--- app/src/leanback/java/com/fongmi/android/tv/ui/custom/CustomTitleView.java
+++ app/src/leanback/java/com/fongmi/android/tv/ui/custom/CustomTitleView.java
@@ -9,27 +9,16 @@ import android.view.animation.Animation;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-import com.fongmi.android.tv.App;
 import com.fongmi.android.tv.R;
-import com.fongmi.android.tv.api.config.VodConfig;
-import com.fongmi.android.tv.bean.Site;
 import com.fongmi.android.tv.impl.SiteListener;
-import com.fongmi.android.tv.ui.activity.HomeActivity;
 import com.fongmi.android.tv.utils.KeyUtil;
 import com.fongmi.android.tv.utils.ResUtil;
 import com.google.android.material.textview.MaterialTextView;
-
-import java.util.List;
 
 public class CustomTitleView extends MaterialTextView {
 
     private Listener listener;
     private Animation flicker;
-    private boolean coolDown;
-
-    private Site getHome() {
-        return VodConfig.get().getHome();
-    }
 
     public CustomTitleView(@NonNull Context context) {
         super(context);
@@ -44,13 +33,13 @@ public class CustomTitleView extends MaterialTextView {
         this.listener = listener;
         setOnClickListener(v -> listener.showDialog());
         setOnLongClickListener(v -> {
-            if(listener != null) listener.onRefresh();
+            if (listener != null) listener.onRefresh();
             return true;
         });
     }
 
     private boolean hasEvent(KeyEvent event) {
-        return !getHome().isEmpty() && (KeyUtil.isLeftKey(event) || KeyUtil.isRightKey(event) || (KeyUtil.isUpKey(event) && !coolDown));
+        return KeyUtil.isLeftKey(event) || KeyUtil.isRightKey(event);
     }
 
     @Override
@@ -68,29 +57,8 @@ public class CustomTitleView extends MaterialTextView {
     }
 
     private void onKeyDown(KeyEvent event) {
-        if (KeyUtil.isActionDown(event) && KeyUtil.isUpKey(event)) onKeyUp();
-        else if (KeyUtil.isActionDown(event) && KeyUtil.isLeftKey(event)) listener.setSite(getSite(false));
-        else if (KeyUtil.isActionDown(event) && KeyUtil.isRightKey(event)) listener.setSite(getSite(true));
-    }
-
-    private void onKeyUp() {
-        App.post(() -> coolDown = false, 3000);
-        if(listener != null) listener.onRefresh();
-        coolDown = true;
-    }
-
-    private Site getSite(boolean next) {
-        List<Site> items = getSites();
-        if (items.isEmpty()) return new Site();
-        int position = items.indexOf(getHome());
-        if (position < 0) position = 0;
-        if (next) position = (position + 1) % items.size();
-        else position = (position - 1 + items.size()) % items.size();
-        return items.get(position);
-    }
-
-    private List<Site> getSites() {
-        return VodConfig.get().getSites().stream().filter(site -> !site.isHide()).toList();
+        if (KeyUtil.isActionDown(event) && KeyUtil.isLeftKey(event)) listener.onTitleLeft();
+        else if (KeyUtil.isActionDown(event) && KeyUtil.isRightKey(event)) listener.onTitleRight();
     }
 
     public interface Listener extends SiteListener {
@@ -100,5 +68,9 @@ public class CustomTitleView extends MaterialTextView {
         void onRefresh();
 
         void reloadConfig();
+
+        void onTitleLeft();
+
+        void onTitleRight();
     }
 }
