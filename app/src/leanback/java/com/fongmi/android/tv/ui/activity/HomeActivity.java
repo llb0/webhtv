8dae86dbb62bac8d9067d719f9fc1b5ef62db3f7
llb0 <108737112+llb0@users.noreply.github.com>
2026-09-10 00:56:00 +0800

    Adjust funcRecycler width calculation in HomeActivity

--- app/src/leanback/java/com/fongmi/android/tv/ui/activity/HomeActivity.java
+++ app/src/leanback/java/com/fongmi/android/tv/ui/activity/HomeActivity.java
@@ -477,7 +477,7 @@ public class HomeActivity extends BaseActivity implements CustomTitleView.Listen
         int count = mFuncAdapter.size();
         if (count > 0) {
             android.view.ViewGroup.LayoutParams params = mBinding.funcRecycler.getLayoutParams();
-            params.width = ResUtil.dp2px(60 * count + 4 * (count - 1));
+            params.width = ResUtil.dp2px(56 * count + 4 * (count - 1) + 60);
             mBinding.funcRecycler.setLayoutParams(params);
         }
     }
