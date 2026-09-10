05e593651aa1d641b662f3b1b74b36832f619310
llb0 <108737112+llb0@users.noreply.github.com>
2026-09-10 19:43:06 +0800

    Fix URL conversion in VodConfig JSON retrieval

--- app/src/main/java/com/fongmi/android/tv/api/config/VodConfig.java
+++ app/src/main/java/com/fongmi/android/tv/api/config/VodConfig.java
@@ -130,7 +130,7 @@ public class VodConfig extends BaseConfig {
         }
         String globalSpider = "";
         try {
-            String json = Decoder.getJson(UrlUtil.convert(config.getUrl()), TAG);
+            String json = Decoder.getJson(config.getUrl(), TAG);
             JsonObject object = Json.parse(json).getAsJsonObject();
             globalSpider = Json.safeString(object, "spider");
             checkJson(config, object);
