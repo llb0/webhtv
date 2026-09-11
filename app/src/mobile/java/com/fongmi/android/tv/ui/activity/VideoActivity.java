b7c317a6611d3631a7c88d82c98068be884597e2
llb0 <108737112+llb0@users.noreply.github.com>
2026-09-11 22:22:00 +0800

    Add cast button functionality in VideoActivity

--- app/src/mobile/java/com/fongmi/android/tv/ui/activity/VideoActivity.java
+++ app/src/mobile/java/com/fongmi/android/tv/ui/activity/VideoActivity.java
@@ -828,6 +828,7 @@ public class VideoActivity extends PlaybackActivity implements Clock.Callback, C
         mBinding.control.action.opening.setOnClickListener(view -> onOpening());
         mBinding.control.action.danmaku.setOnClickListener(view -> onDanmaku());
         mBinding.control.action.episodes.setOnClickListener(view -> onEpisodes());
+        mBinding.control.action.cast.setOnClickListener(view -> onCast());
         mBinding.control.action.setting.setOnClickListener(view -> {
             mWasPlayingBeforeSetting = service() != null && player().isPlaying();
             if (mWasPlayingBeforeSetting) onPaused();
@@ -1039,6 +1040,7 @@ public class VideoActivity extends PlaybackActivity implements Clock.Callback, C
         addActionButton(PlayerButtonSetting.PREV, mBinding.control.action.prev);
         addActionButton(PlayerButtonSetting.NEXT, mBinding.control.action.next);
         addActionButton(PlayerButtonSetting.EPISODES, mBinding.control.action.episodes);
+        addActionButton(PlayerButtonSetting.CAST, mBinding.control.action.cast);
         addActionButton(PlayerButtonSetting.SETTING, mBinding.control.action.setting);
         PlayerButtonSetting.applyOrder(mBinding.control.action.container, mActionButtons);
         setupCustomActionButtons();
