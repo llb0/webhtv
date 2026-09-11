f9ea75e43cb350b597d7d608489b6fecc4f867cc
llb0 <108737112+llb0@users.noreply.github.com>
2026-09-11 15:46:13 +0800

    Adjust progress layout top offset and startup action

--- app/src/leanback/java/com/fongmi/android/tv/ui/activity/HomeActivity.java
+++ app/src/leanback/java/com/fongmi/android/tv/ui/activity/HomeActivity.java
@@ -200,7 +200,7 @@ public class HomeActivity extends BaseActivity implements CustomTitleView.Listen
         if (mBinding.nativeContent.getPaddingTop() != top) {
             mBinding.nativeContent.setPadding(mBinding.nativeContent.getPaddingLeft(), top, mBinding.nativeContent.getPaddingRight(), mBinding.nativeContent.getPaddingBottom());
         }
-        mBinding.progressLayout.setProgressTopOffset(top * 2);
+        mBinding.progressLayout.setProgressTopOffset(top / 2);
     }
 
     private void syncWebOverlayLayout() {
@@ -351,6 +351,7 @@ public class HomeActivity extends BaseActivity implements CustomTitleView.Listen
 
     private void runStartupAction() {
         if (mStartupActionDone) return;
+        if (shouldSkipStartup()) return;
         mStartupActionDone = true;
         switch (Setting.getDefaultLaunch()) {
             case Setting.DEFAULT_LAUNCH_LIVE:
