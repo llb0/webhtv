11bd557cf21ff9f3d56068f61ead97b7b275a7df
llb0 <108737112+llb0@users.noreply.github.com>
2026-09-12 14:48:49 +0800

    Adjust focus request based on render visibility

--- app/src/leanback/java/com/fongmi/android/tv/ui/activity/SettingPlayerActivity.java
+++ app/src/leanback/java/com/fongmi/android/tv/ui/activity/SettingPlayerActivity.java
@@ -69,7 +69,11 @@ public class SettingPlayerActivity extends BaseActivity implements UaListener, B
         setVisible();
         format = new DecimalFormat("0.#");
         PlaybackPerformanceSetting.ensureInitialized();
-        mBinding.exo4kCompat.requestFocus();
+        if (mBinding.render.getVisibility() == View.VISIBLE) {
+            mBinding.render.requestFocus();
+        } else {
+            mBinding.playerButtons.requestFocus();
+        }
         mBinding.uaText.setText(Setting.getUa());
         mBinding.aacText.setText(getSwitch(PlayerSetting.isPreferAAC()));
         mBinding.tunnelText.setText(getSwitch(PlayerSetting.isTunnel()));
