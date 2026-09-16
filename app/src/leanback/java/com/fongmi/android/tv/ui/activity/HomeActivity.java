c9ff4d8af2478f4bea718dfb52a6173d679f10dd
llb0 <108737112+llb0@users.noreply.github.com>
2026-09-16 09:59:15 +0800

    返回直接退出，不进后台Adjust layout parameters and simplify exit logic

--- app/src/leanback/java/com/fongmi/android/tv/ui/activity/HomeActivity.java
+++ app/src/leanback/java/com/fongmi/android/tv/ui/activity/HomeActivity.java
@@ -483,7 +483,7 @@ public class HomeActivity extends BaseActivity implements CustomTitleView.Listen
         int count = mFuncAdapter.size();
         if (count > 0) {
             android.view.ViewGroup.LayoutParams params = mBinding.funcRecycler.getLayoutParams();
-            params.width = ResUtil.dp2px(64 * count + 4 * (count - 1) + 68);
+            params.width = ResUtil.dp2px(66 * count + 4 * (count - 1) + 70);
             mBinding.funcRecycler.setLayoutParams(params);
         }
     }
@@ -927,8 +927,7 @@ public class HomeActivity extends BaseActivity implements CustomTitleView.Listen
     }
 
     private void confirmExitHome() {
-        if (PlaybackService.isRunning()) Util.moveToBackground(this);
-        else super.onBackInvoked();
+        super.onBackInvoked();
     }
 
     @Override
