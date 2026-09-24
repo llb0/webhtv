971050bc0ff6f67665c0b4ee9907059241432bfc
llb0 <108737112+llb0@users.noreply.github.com>
2026-09-24 10:52:50 +0800

    Update SettingPlayerActivity invocation to include flag

--- app/src/leanback/java/com/fongmi/android/tv/ui/activity/VideoActivity.java
+++ app/src/leanback/java/com/fongmi/android/tv/ui/activity/VideoActivity.java
@@ -737,7 +737,7 @@ public class VideoActivity extends PlaybackActivity implements CustomKeyDownVod.
         mBinding.control.action.change2.setOnClickListener(view -> onChange());
         mBinding.control.action.fullscreen.setOnClickListener(view -> onFullscreen());
         mBinding.control.action.danmaku.setOnClickListener(view -> onDanmaku());
-        mBinding.control.action.setting.setOnClickListener(view -> SettingPlayerActivity.start(this));
+        mBinding.control.action.setting.setOnClickListener(view -> SettingPlayerActivity.start(this, true));
         mBinding.control.action.cast.setOnClickListener(view -> onCast());
         mBinding.control.action.timer.setOnClickListener(view -> onTimer());
         mBinding.control.action.opening.setOnClickListener(view -> onOpening());
@@ -6089,7 +6089,7 @@ public class VideoActivity extends PlaybackActivity implements CustomKeyDownVod.
         if (isVisible(mBinding.lutQuick)) return dispatchLutQuickKey(event);
         if (KeyUtil.isMenuKey(event)) {
             if (isFullscreen()) {
-                if (isVisible(mBinding.control.getRoot())) SettingPlayerActivity.start(this);
+                if (isVisible(mBinding.control.getRoot())) SettingPlayerActivity.start(this, true);
                 else onToggle();
             } else {
                 onContent();
